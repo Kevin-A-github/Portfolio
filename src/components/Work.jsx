8c5fa92befba2eb04afe9c469d7e97ba3b428dc6
Kevin <kabreu11385@gmail.COM>
2022-08-18 18:34:16 -0400

    projects components

---
+++ src/components/Work.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import simon from '../assets/simon.jpg';
+import breaking from '../assets/breaking.jpg';
+import whitebg from '../assets/whitebg.jpg';
+
+const Work = () => {
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f] ">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justiy-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">//Check out my projects</p>
+        </div>
+
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${simon})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-gray-700 tracking-wider">
+                Simon Game
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://famous-griffin-c87240.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Kevin-A-github/Simon-Game">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${breaking})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-gray-700 tracking-wider">
+                Breaking Bad Fan Site
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://cosmic-madeleine-0f596a.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Kevin-A-github/ReactAPI">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${whitebg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-gray-700 tracking-wider">
+                Chatty
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo Coming Soon
+                  </button>
+                </a>
+                <a href="https://github.com/Kevin-A-github/chat-frontend">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code Available
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
